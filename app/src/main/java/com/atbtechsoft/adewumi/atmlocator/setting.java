e56215031f87a297125ba8461d0a06c58932929c
Adeola Mercy <mercycharis44@gmail.com>
2014-10-14 16:14:05 +0100

    settings screen,profie screen,notification screen,about us screen

---
+++ app/src/main/java/com/atbtechsoft/adewumi/atmlocator/setting.java
@@ -0,0 +1,7 @@
+package com.atbtechsoft.adewumi.atmlocator;
+
+/**
+ * Created by ADEWUMI on 10/14/2014.
+ */
+public class Setting {
+}
--- app/src/main/java/notification.java
+++ app/src/main/java/notification.java
@@ -1,5 +1,8 @@
+import android.app.Activity;
+
 /**
  * Created by ADEWUMI on 10/14/2014.
  */
-public class notification {
+public class notification extends Activity {
+
 }
