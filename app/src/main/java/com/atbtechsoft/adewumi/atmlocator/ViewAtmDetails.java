4fb10a5171e8b42617fc1b15e39033e1416e4a38
Adeola Mercy <mercycharis44@gmail.com>
2014-10-20 14:24:11 +0100

    help screen modified....made texts clickable

--- app/src/main/java/com/atbtechsoft/adewumi/atmlocator/ViewAtmDetails.java
+++ app/src/main/java/com/atbtechsoft/adewumi/atmlocator/ViewAtmDetails.java
@@ -1,9 +1,11 @@
 package com.atbtechsoft.adewumi.atmlocator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by ADEWUMI on 10/20/2014.
@@ -20,6 +22,12 @@ public class ViewAtmDetails extends ActionBarActivity{
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.viewatm,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent detailsIntent = new Intent(getApplicationContext(),HomeScreen.class);
+        startActivityForResult(detailsIntent,0);
         return true;
     }
 }
